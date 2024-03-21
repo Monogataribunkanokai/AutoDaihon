f8f4c240a11fd9666e0da452c5093c84e4df5f8f
bacon <bacon0817.ix@gmail.com>
2024-03-21 15:22:27 +0900

    作者を取得

--- python/GetAozora.py
+++ python/GetAozora.py
@@ -1,9 +1,14 @@
 import requests
 from typing import Union
 from html.parser import HTMLParser
+from bs4 import BeautifulSoup
 
 def get_HTML(url:str='https://www.aozora.gr.jp/cards/000081/files/473_42318.html',file_name:str|bool=False):
     site_data=requests.get(url)
     site_data.encoding=site_data.apparent_encoding
+    soup=BeautifulSoup(site_data.text,'html.parser')
+    titel_text=soup.find('meta',attrs= {'name':'DC.Title'}).get('content')
+    print(titel_text)
     with open('test.html','w',encoding='utf-8') as f:
         f.write(site_data.text)
+get_HTML()
