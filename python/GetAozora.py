7295211a864674ade8fad292471ace119791080b
bacon <bacon0817.ix@gmail.com>
2024-03-21 13:02:46 +0900

    HTMLパース準備

--- python/GetAozora.py
+++ python/GetAozora.py
@@ -1,6 +1,9 @@
 import requests
-url='https://www.aozora.gr.jp/cards/000081/files/473_42318.html'
-site_data=requests.get(url)
-site_data.encoding=site_data.apparent_encoding
-with open('test.html','w',encoding='utf-8') as f:
-    f.write(site_data.text)
+from typing import Union
+from html.parser import HTMLParser
+
+def get_HTML(url:str='https://www.aozora.gr.jp/cards/000081/files/473_42318.html',file_name:str|bool=False):
+    site_data=requests.get(url)
+    site_data.encoding=site_data.apparent_encoding
+    with open('test.html','w',encoding='utf-8') as f:
+        f.write(site_data.text)
