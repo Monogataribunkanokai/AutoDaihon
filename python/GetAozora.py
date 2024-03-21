3f26b765b0dc0c66db0161f7053fe795842e5ba3
bacon <bacon0817.ix@gmail.com>
2024-03-21 15:51:22 +0900

    本部取得

--- python/GetAozora.py
+++ python/GetAozora.py
@@ -2,21 +2,36 @@ import requests
 from typing import Union
 from html.parser import HTMLParser
 from bs4 import BeautifulSoup
-
-def get_HTML_soup(url:str='https://www.aozora.gr.jp/cards/000081/files/473_42318.html',file_name:str|bool=False):
-    site_data=requests.get(url)
-    site_data.encoding=site_data.apparent_encoding
-    soup=BeautifulSoup(site_data.text,'html.parser')
-    return soup
-class AnalyzeHTML():
-    def __init__(self,soup) -> None:
-        self.__soup=soup
+import os
+class GetHTML():
+    def __init__(self,url:str) -> None:
+        self.__url=url
+        self.__soup=self.__get_HTML_soup()
+        self.__site_data=self.__get_HTML_data()
+    def __get_HTML_data(self):
+        site_data=requests.get(self.__url)
+        site_data.encoding=site_data.apparent_encoding
+        return site_data
+    def __get_HTML_soup(self):
+        site_data=self.__get_HTML_data()
+        soup=BeautifulSoup(site_data.text,'html.parser')
+        return soup
+    def download_HTML_file(self,save_path:str='',file_name:str|bool=False):
+        if type(file_name)==bool and file_name==False:
+            save_path=os.path.join(save_path,str(self.get_title())+'.html')
+        else:
+            save_path=os.path.join(save_path,str(file_name))
+        with open(save_path,mode='w') as file:
+            file.write(self.__site_data.text)
     def get_title(self):
         titel_text=self.__soup.find('meta',attrs= {'name':'DC.Title'}).get('content')
         return titel_text
     def get_creator(self):
         titel_text=self.__soup.find('meta',attrs= {'name':'DC.Creator'}).get('content')
         return titel_text
-soup=get_HTML_soup()
-html_text=AnalyzeHTML(soup=soup)
-print(html_text.get_creator())
+    def get_main_text(self):
+        main_text=self.__soup.find('div',class_='main_text')
+        return main_text
+url:str='https://www.aozora.gr.jp/cards/000081/files/473_42318.html'
+g=GetHTML(url=url)
+print(g.get_main_text())
